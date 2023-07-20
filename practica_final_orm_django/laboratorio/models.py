a414fe9c90b23b8be1b75bf49ef64b5e069e14b0
Alejandra <informaticotrabajo00@gmail.com>
2023-07-20 19:20:12 -0400

    model

--- practica_final_orm_django/laboratorio/models.py
+++ practica_final_orm_django/laboratorio/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Laboratorio(models.Model):
+    nombre = models.CharField(max_length=200, verbose_name="Laboratorio")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de Creación")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de actualización")
+
+    class Meta:
+        verbose_name = "Laboratorio"
+        verbose_name_plural = "Laboratorios"
+
+    def __str__(self):
+        return self.nombre
+
+
+class DirectorGeneral(models.Model):
+    nombre = models.CharField(max_length=255, verbose_name="Director General")
+    laboratorio = models.OneToOneField("Laboratorio", on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de Creación")
+    updated = models.DateTimeField(
+        auto_now=True, verbose_name="Fecha de actuialización"
+    )
+
+    class Meta:
+        verbose_name = "DirectorGeneral"
+        verbose_name_plural = "DirectoresGenerales"
+
+    def __str__(self):
+        return self.nombre
+
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=255, verbose_name="Producto")
+    laboratorio = models.ForeignKey(
+        "Laboratorio", on_delete=models.SET_NULL, blank=True, null=True
+    )
+    f_fabricacion = models.DateField(verbose_name="Fecha de Fabricacion")
+    p_costo = models.DecimalField(
+        null=False, decimal_places=2, max_digits=10, verbose_name="Precio de Costo"
+    )
+    p_venta = models.DecimalField(
+        null=False, decimal_places=2, max_digits=10, verbose_name="Precio de Venta"
+    )
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de Creación")
+    updated = models.DateTimeField(
+        auto_now=True, verbose_name="Fecha de actuialización"
+    )
+
+    class Meta:
+        verbose_name = "Producto"
+        verbose_name_plural = "Productos"
+
+    def __str__(self):
+        return self.nombre
