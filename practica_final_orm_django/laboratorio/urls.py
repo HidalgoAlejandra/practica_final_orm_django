92b1161b5fa8b428c647902764af04517d28a24b
Alejandra <informaticotrabajo00@gmail.com>
2023-07-30 19:32:11 -0400

    login

--- practica_final_orm_django/laboratorio/urls.py
+++ practica_final_orm_django/laboratorio/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.insertar_lab_view, name="insertar_lab"),
+    path("", views.index, name="index"),
+    path("insertar/", views.insertar_lab_view, name="insertar_lab"),
     path("mostrar/", views.mostrar_lab_view, name="mostrar_lab"),
     path("editar/<int:pk>/", views.editar_lab_view, name="editar_lab"),
     path("eliminar/<int:pk>/", views.eliminar_lab_view, name="eliminar_lab"),
@@ -12,4 +13,6 @@ urlpatterns = [
         views.laboratorio_detalle_view,
         name="laboratorio_detalle",
     ),
+    path("logout/", views.logout_view, name="logout"),
+    path("login/", views.login_view, name="login"),
 ]
--- practica_final_orm_django/laboratorio/views.py
+++ practica_final_orm_django/laboratorio/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect, HttpResponse, HttpResponseRedirect
 from .forms import LaboratorioForm
 from .models import Laboratorio
+
+from django.contrib import messages
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import AuthenticationForm
 
 
 # Create your views here.
@@ -17,8 +21,11 @@ def insertar_lab_view(request):
 
 def mostrar_lab_view(request):
     laboratorios = Laboratorio.objects.all()
+    num_visitas = request.session.get("num_visits", 1)
+    request.session["num_visits"] = num_visitas + 1
     context = {
         "laboratorios": laboratorios,
+        "num_visitas": num_visitas,
     }
     return render(request, "laboratorio/mostrar.html", context)
 
@@ -54,3 +61,35 @@ def laboratorio_detalle_view(request, pk):
     laboratorio = Laboratorio.objects.get(id=pk)
     context = {"laboratorio": laboratorio}
     return render(request, "laboratorio/detalle.html", context=context)
+
+
+def logout_view(request):
+    logout(request)
+    messages.info(request, "Seha cerrado la sesion satisfactoriamente.")
+    return HttpResponseRedirect("/")
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"iniciaste sesion como: {username}.")
+                return HttpResponseRedirect("/laboratorio/mostrar")
+            else:
+                messages.error(request, "username o password Incorrectos")
+                return HttpResponseRedirect("/laboratorio/login")
+        else:
+            messages.error(request, "username o password Incorrectos")
+            return HttpResponseRedirect("/laboratorio/login")
+    form = AuthenticationForm()
+    context = {"login_form": form}
+    return render(request, "laboratorio/login.html", context)
+
+
+def index(request):
+    return render(request, "laboratorio/index.html")
