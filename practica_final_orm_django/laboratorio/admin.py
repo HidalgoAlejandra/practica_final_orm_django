527c24ef054e0614c3665015864a9f31d6067c78
Alejandra <informaticotrabajo00@gmail.com>
2023-07-29 22:08:29 -0400

    ingreso datos

--- practica_final_orm_django/laboratorio/admin.py
+++ practica_final_orm_django/laboratorio/admin.py
@@ -7,10 +7,31 @@ from .models import Laboratorio, DirectorGeneral, Producto
 class LaboratorioAdmin(admin.ModelAdmin):
     search_fields = ["nombre"]
     list_display = ["id", "nombre"]
+    ordering = ["nombre"]
+
+
+class DirectorGeneralAdmin(admin.ModelAdmin):
+    search_fields = ["nombre"]
+    list_display = ["id", "nombre", "laboratorio"]
+    ordering = ["nombre"]
+
+
+class ProductoAdmin(admin.ModelAdmin):
+    search_fields = ["nombre", "laboratorio"]
+    list_display = [
+        "id",
+        "nombre",
+        "laboratorio",
+        "f_fabricacion",
+        "p_costo",
+        "p_venta",
+    ]
+    ordering = ["nombre"]
+    list_filter = ("nombre", "laboratorio")
 
 
 admin.site.register(Laboratorio, LaboratorioAdmin)
 
-admin.site.register(DirectorGeneral)
+admin.site.register(DirectorGeneral, DirectorGeneralAdmin)
 
-admin.site.register(Producto)
+admin.site.register(Producto, ProductoAdmin)
--- practica_final_orm_django/laboratorio/models.py
+++ practica_final_orm_django/laboratorio/models.py
@@ -1,3 +1,4 @@
+import datetime
 from django.db import models
 
 
@@ -31,12 +32,25 @@ class DirectorGeneral(models.Model):
         return self.nombre
 
 
+annos_choices = []
+
+for r in range(1999, (datetime.datetime.now().year + 1)):
+    annos_choices.append((r, r))
+
+
+def anno_actual():
+    return datetime.date.today().year
+
+
 class Producto(models.Model):
-    nombre = models.CharField(max_length=255, verbose_name="Producto")
+    nombre = models.CharField(max_length=255, verbose_name="Nombre")
     laboratorio = models.ForeignKey(
         "Laboratorio", on_delete=models.SET_NULL, blank=True, null=True
     )
-    f_fabricacion = models.DateField(verbose_name="Fecha de Fabricacion")
+    f_fabricacion = models.IntegerField(
+        choices=annos_choices, default=anno_actual, verbose_name="Fecha de Fabricacion"
+    )
+    # f_fabricacion = models.DateField(verbose_name="Fecha de Fabricacion")
     p_costo = models.DecimalField(
         null=False, decimal_places=2, max_digits=10, verbose_name="Precio de Costo"
     )
